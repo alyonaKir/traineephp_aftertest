a947630300163068f0b0b23e497a9bae1a161ce5
Alyona Kirushcheva <alyona.kirushcheva@gmail.com>
2022-11-29 10:44:02 +0100

    twig

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -1,9 +1,13 @@
 <?php
 namespace App\controllers;
+include 'vendor/autoload.php';
+use Twig\Loader\FilesystemLoader;
+
 class AppController
 {
     public function index(): void
     {
+        $loader = new FilesystemLoader('/app/views');
         require "app/views/mainPage.php";
     }
 }
