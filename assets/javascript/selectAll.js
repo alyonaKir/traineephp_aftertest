67f14a945c6447eb95c7694f082af4c322c40d16
Alyona Kirushcheva <alyona.kirushcheva@gmail.com>
2022-12-05 14:59:41 +0100

    last change

--- assets/javascript/selectAll.js
+++ assets/javascript/selectAll.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
-    if(localStorage.getItem('flag')){
-        $('#select_all').prop('checked',true);
-    }
-    var allCheck = document.getElementById('select_all');
-    alert(allCheck.checked);
-    if(allCheck.checked === true){
-        localStorage.setItem('flag', true);
+    // if(localStorage.getItem('flag')){
+    //     $('#select_all').prop('checked',true);
+    // }
+
+    $('#select_all').on('click',function(){
         if(this.checked){
             $('.checkbox').each(function(){
+                localStorage.setItem('flag', true);
                 this.checked = true;
             });
         }else{
@@ -16,26 +15,12 @@ $(document).ready(function(){
                 this.checked = false;
             });
         }
-    }
-
-    // $('#select_all').on('click',function(){
-    //     if(this.checked){
-    //         $('.checkbox').each(function(){
-    //             localStorage.setItem('flag', true);
-    //             this.checked = true;
-    //         });
-    //     }else{
-    //         localStorage.setItem('flag', false);
-    //         $('.checkbox').each(function(){
-    //             this.checked = false;
-    //         });
-    //     }
-    // });
-    // $('.checkbox').on('click',function(){
-    //     if($('.checkbox:checked').length == $('.checkbox').length){
-    //         $('#select_all').prop('checked',true);
-    //     }else{
-    //         $('#select_all').prop('checked',false);
-    //     }
-    // });
+    });
+    $('.checkbox').on('click',function(){
+        if($('.checkbox:checked').length == $('.checkbox').length){
+            $('#select_all').prop('checked',true);
+        }else{
+            $('#select_all').prop('checked',false);
+        }
+    });
 });
