079bdb9c9a61cb3e57bd62670a336436d79fae5f
Alyona Kirushcheva <alyona.kirushcheva@gmail.com>
2022-12-19 12:16:26 +0100

    rest

--- database/RestDBClass.php
+++ database/RestDBClass.php
@@ -7,15 +7,24 @@ use Models\User;
 class RestDBClass
 {
     private $dataCount;
+    private $headers = array();
 
     public function __construct()
     {
-        //$allData = file_get_contents("https://gorest.co.in/public/v2/users");
+        $this->headers[] = 'Accept: application/json';
+        $this->headers[] = 'Content-Type: application/json';
+        $this->headers[] = 'Authorization: Bearer 9793ead2cc8ff849a69a00ffe49b8abc391f4de0398a79ce9bdccd8beef30cb6';
     }
 
     public function getUserById($id) : User
     {
-        $user = file_get_contents("https://gorest.co.in/public/v2/users/" . $id);
+        $ch = curl_init('https://gorest.co.in/public/v2/users/'.$id);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+        $user = curl_exec($ch);
+        curl_close($ch);
+
         $jsonArray = json_decode($user, true);
         $userObj = new User($jsonArray['email'], $jsonArray['name'], $jsonArray['gender'], $jsonArray['status'], "rest_api");
         $userObj->setId($id);
@@ -26,22 +35,14 @@ class RestDBClass
     public function showAllUsers($pageno, $size_page): array
     {
         $users = array();
-
-        $headers = array();
-        $headers[] = 'Accept: application/json';
-        $headers[] = 'Content-Type: application/json';
-        $headers[] = 'Authorization: Bearer 9793ead2cc8ff849a69a00ffe49b8abc391f4de0398a79ce9bdccd8beef30cb6';
-
-
         $ch = curl_init('https://gorest.co.in/public/v2/users?page='.$pageno.'&per_page='.$size_page);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
         $allData = curl_exec($ch);
         curl_close($ch);
         $jsonArray = json_decode($allData, true);
 
-        echo count($jsonArray);
         for($i=0; $i < count($jsonArray); $i++) {
             $users[] = new User($jsonArray[$i]["email"], $jsonArray[$i]["name"], $jsonArray[$i]["gender"], $jsonArray[$i]["status"], "rest_api");
             $users[count($users) - 1]->setId($jsonArray[$i]['id']);
@@ -51,15 +52,11 @@ class RestDBClass
 
     public function deleteUserById($id)
     {
-        $headers = array();
-        $headers[] = 'Accept: application/json';
-        $headers[] = 'Content-Type: application/json';
-        $headers[] = 'Authorization: Bearer 9793ead2cc8ff849a69a00ffe49b8abc391f4de0398a79ce9bdccd8beef30cb6';
 
         $ch = curl_init('https://gorest.co.in/public/v2/users/'.$id);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
         curl_exec($ch);
         curl_close($ch);
     }
@@ -72,15 +69,10 @@ class RestDBClass
             'status' =>"active"
         );
         $data_json =  json_encode($data);
-        $headers = array();
-        $headers[] = 'Accept: application/json';
-        $headers[] = 'Content-Type: application/json';
-        $headers[] = 'Content-Length: ' . strlen($data_json);
-        $headers[] = 'Authorization: Bearer 3ed62aaac5b04a7c41a8003628aded79288ebd8ea58eaf42dbc5323b9aba57be';
 
         $curl = curl_init('https://gorest.co.in/public/v2/users/'.$user->getId());
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $this->headers);
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data_json);
 
@@ -90,10 +82,6 @@ class RestDBClass
 
     public function addUser(User $user): void
     {
-        $headers = array();
-        $headers[] = 'Accept: application/json';
-        $headers[] = 'Content-Type: application/json';
-        $headers[] = 'Authorization: Bearer 9793ead2cc8ff849a69a00ffe49b8abc391f4de0398a79ce9bdccd8beef30cb6';
         $data = array(
             "name" => $user->getName(),
             "gender" => $user->getGender(),
@@ -104,7 +92,7 @@ class RestDBClass
         curl_setopt($ch1, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch1, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch1, CURLOPT_POSTFIELDS, json_encode($data));
-        curl_setopt($ch1, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch1, CURLOPT_HTTPHEADER, $this->headers);
         curl_exec($ch1);
         curl_close($ch1);
     }
